470978fa804b23fdf66d33a0fd3a774da3ddc329
Benjamin <Benjaminneyt@gmail.com>
2023-09-30 17:50:26 +0200

    made static html for cal

---
+++ script.js
@@ -0,0 +1,21 @@
+let firstNum = null;
+let operator = null;
+let secondNum = null;
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(5, 5))
